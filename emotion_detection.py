ae3ab39d07ebb4708a4ccdc3a9b2c859d2d4358c
cheukhongip <cheukhongip207@gmail.com>
2025-03-05 17:17:01 -0500

    Updated Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,5 +8,10 @@ def emotion_detector(text_to_analyze):
     json_format =  { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=json_format, headers=header) 
+    formatted_response = json.loads(response.text) 
 
-    return response.text
+    emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
+    
+    dominant_emotion =  max(emotion_dict, key=emotion_dict.get)
+    
+    return {**emotion_dict, 'dominant_emotion': dominant_emotion}
